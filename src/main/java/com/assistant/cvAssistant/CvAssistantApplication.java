6c80fe88c3709ad2cdb7387752c7ba994793c4a2
MrKotula <bohdanbilyk1001@gmail.com>
2025-07-29 17:05:23 +0200

    Initial commit

---
+++ src/main/java/com/assistant/cvAssistant/CvAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.assistant.cvAssistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvAssistantApplication.class, args);
+	}
+
+}
